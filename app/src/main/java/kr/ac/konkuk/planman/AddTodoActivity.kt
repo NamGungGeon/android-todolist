c4a639e7e1fc2caf00367b925e472747583b4152
unknown <mattyoon99@gmail.com>
2021-05-23 02:14:13 +0900

    todo 추가 및 수정 UI 완료

---
+++ app/src/main/java/kr/ac/konkuk/planman/AddTodoActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.konkuk.planman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTodoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_todo)
+    }
+}
