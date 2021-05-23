6f31b862a53b2775a352f5551dc273cad6059879
namgung-geon <rndrjs123@naver.com>
2021-05-23 21:02:05 +0900

    Add MainActivity (TodoFragment Wrapper)

--- app/src/main/java/kr/ac/konkuk/planman/MainActivity.kt
+++ app/src/main/java/kr/ac/konkuk/planman/MainActivity.kt
@@ -1,11 +1,36 @@
 package kr.ac.konkuk.planman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kr.ac.konkuk.planman.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    val todoListVisualizers:ArrayList<Fragment> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+        todoListVisualizers.add(CalendarTodoFragment())
+        todoListVisualizers.add(CalendarTodoFragment())
+        todoListVisualizers.add(CalendarTodoFragment())
+
+        binding.apply {
+            todoListPager.adapter= object: FragmentStateAdapter(this@MainActivity){
+                override fun getItemCount(): Int {
+                    return todoListVisualizers.size
+                }
+
+                override fun createFragment(position: Int): Fragment {
+                    return todoListVisualizers[position]
+                }
+            }
+            todoListPager.offscreenPageLimit= todoListVisualizers.size
+        }
     }
 }
