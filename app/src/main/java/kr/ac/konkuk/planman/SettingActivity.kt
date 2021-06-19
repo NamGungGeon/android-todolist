589eb73c2943fc27bbd364bf960e75a29f2944fc
GeonNamGung <rndrjs123@naver.com>
2021-06-19 21:14:45 +0900

    add reset mene to SettingActivity

--- app/src/main/java/kr/ac/konkuk/planman/SettingActivity.kt
+++ app/src/main/java/kr/ac/konkuk/planman/SettingActivity.kt
@@ -1,8 +1,10 @@
 package kr.ac.konkuk.planman
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import kr.ac.konkuk.planman.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
@@ -21,10 +23,23 @@ class SettingActivity : AppCompatActivity() {
     private fun initSettingMenuList() {
         val menuList = ArrayList<SimpleListRecyclerViewAdapter.SimpleListItem>()
         menuList.add(SimpleListRecyclerViewAdapter.SimpleListItem("카테고리 관리", {
-            val intent= Intent(this, CategoryListActivity::class.java)
+            val intent = Intent(this, CategoryListActivity::class.java)
             startActivity(intent)
         }))
         menuList.add(SimpleListRecyclerViewAdapter.SimpleListItem("알림 설정"))
+        menuList.add(SimpleListRecyclerViewAdapter.SimpleListItem("초기화", {
+            AlertDialog.Builder(this@SettingActivity)
+                .setTitle("초기화")
+                .setMessage("모든 할 일, 카테고리 정보를 초기화합니다\n계속하시겠습니까?")
+                .setPositiveButton("초기화") { dialog, i ->
+                    DB(applicationContext).resetDB()
+                    dialog.dismiss()
+                    Toast.makeText(applicationContext, "초기화 되었습니다", Toast.LENGTH_SHORT).show()
+                }
+                .setNegativeButton("닫기") { dialog, i ->
+                    dialog.dismiss()
+                }.create().show()
+        }))
 
         val listFragment =
             supportFragmentManager.findFragmentById(R.id.setting_menu_list) as SimpleListFragment
