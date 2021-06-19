47d72d2a78b871cb95119a6fc755f9e750b5cb6d
NamGungGeon <rndrjs123@naver.com>
2021-06-20 02:39:21 +0900

    add SplashActivity

---
+++ app/src/main/java/kr/ac/konkuk/planman/SplashActivity.kt
@@ -0,0 +1,25 @@
+package kr.ac.konkuk.planman
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        supportActionBar?.hide()
+
+        Thread{
+            Thread.sleep(1500)
+
+            if(!isDestroyed){
+                runOnUiThread{
+                    val intent= Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }.start()
+    }
+}
